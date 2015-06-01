f660af1c3af9a73a02d842efd73624bd673badc3
Jonathan Clem <jotclem@gmail.com>
2015-05-31 22:52:17 -0400

    Add roundtrip tests

--- test/logfmt_test.exs
+++ test/logfmt_test.exs
@@ -1,4 +1,17 @@
 defmodule LogfmtTest do
   use ExUnit.Case
+
   doctest Logfmt
+
+  test "roundtrip key unquoted value pair" do
+    assert roundtrip("foo=bar") == "foo=bar"
+  end
+
+  test "roundtrip key quoted value pair" do
+    assert roundtrip(~s(foo="bar baz")) == ~s(foo="bar baz")
+  end
+
+  defp roundtrip(string) do
+    string |> Logfmt.decode |> Logfmt.encode
+  end
 end
